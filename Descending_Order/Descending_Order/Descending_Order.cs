452f1ca29c69a9a3ef7532983a70c6e77c4702be
James992927108 <james992927108@gmail.com>
2017-07-23 10:59:48 +0800

    Descending_Order_Test

---
+++ Descending_Order/Descending_Order/Descending_Order.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace Descending_Order
+{
+    class Descending_Order
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string strNum = string.Empty;
+                int Num = 0;
+                do
+                {
+                    Console.Write("Please Enter the Number : ");
+                    strNum = Console.ReadLine();
+                } while (!Int32.TryParse(strNum, out Num));
+                Console.WriteLine(string.Format("{0} -> {1}", strNum, DescendingOrder(Num)));
+            }
+        }
+
+        public static long DescendingOrder(long actual_Input)
+        {
+            long AfterSortLong = 0;
+            string AfterSortString = "";
+            int[] arrayList = actual_Input.ToString().Select(o => Convert.ToInt32(o - 48)).ToArray();
+            Array.Sort(arrayList);
+            Array.Reverse(arrayList);
+            for (int i = 0; i < arrayList.Length; i++)
+            {
+                AfterSortString += arrayList[i].ToString();
+            }
+            AfterSortLong = Convert.ToInt64(AfterSortString);
+            return AfterSortLong;
+        }
+    }
+
+}
