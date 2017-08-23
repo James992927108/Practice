48fd75d504c9452157c26c6bc58c2eaeef6cdff0
Jazerzxc <james992927108@gmail.com>
2017-08-23 14:39:14 +0800

    unfinish

---
+++ 19_Merged_String_Checker/19_Merged_String_Checker/Test.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+namespace _Merged_String_Checker
+{
+	[TestFixture]
+	public class StringMergerTestsㄇ
+	{
+		[Test]
+		public void HappyPath1()
+		{
+			Assert.IsTrue(StringMerger.isMerge("codewars", "code", "wars"), "codewars can be created from code and wars");
+		}
+
+		[Test]
+		public void HappyPath2()
+		{
+			Assert.IsTrue(StringMerger.isMerge("codewars", "cdwr", "oeas"), "codewars can be created from cdwr and oeas");
+		}
+
+		[Test]
+		public void SadPath1()
+		{
+			Assert.IsFalse(StringMerger.isMerge("codewars", "cod", "wars"), "Codewars are not codwars");
+		}
+	}
+	public class StringMerger
+	{
+		public static bool isMerge(string s, string part1, string part2)
+		{
+			return false;
+		}
+	}
+	
+}
+
+
