d3687221ec0dd50601b60be89397ffbd90dea221
Jazerzxc <james992927108@gmail.com>
2017-08-26 15:38:12 +0800

    finish

--- 19_Merged_String_Checker/19_Merged_String_Checker/Test.cs
+++ 19_Merged_String_Checker/19_Merged_String_Checker/Test.cs
@@ -1,9 +1,11 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Linq;
+
 namespace _Merged_String_Checker
 {
 	[TestFixture]
-	public class StringMergerTestsㄇ
+	public class StringMergerTests
 	{
 		[Test]
 		public void HappyPath1()
@@ -22,12 +24,27 @@ namespace _Merged_String_Checker
 		{
 			Assert.IsFalse(StringMerger.isMerge("codewars", "cod", "wars"), "Codewars are not codwars");
 		}
-	}
 	public class StringMerger
 	{
-		public static bool isMerge(string s, string part1, string part2)
-		{
-			return false;
+		public static bool isMerge(String s, String part1, String part2) {
+			if (part1 == part2 & s!="") return false;
+
+			string[] get_s_element = s.Select(o => o.ToString()).ToArray();
+			string[] get_part1_element = part1.Select(o => o.ToString()).ToArray();
+			string[] get_part2_element = part2.Select(o => o.ToString()).ToArray();
+
+			int l = get_s_element.Length, l1 = get_part1_element.Length, l2 = get_part2_element.Length;
+			int n = 0, m = 0;
+			if (l == (l1 + l2)) {
+				for (int i = 0; i < l; i++) {
+					if (n < l1 && get_s_element[i] == get_part1_element[n]) n++;
+					if (m < l2 && get_s_element[i] == get_part2_element[m]) m++;
+				}
+				if (l == n + m) {
+					return true;
+				} else return false;
+
+			} else return false;
 		}
 	}
 	
