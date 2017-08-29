ef2692357a004b9ff2bf0a1e5c047560365734f3
Jazerzxc <james992927108@gmail.com>
2017-08-29 17:39:56 +0800

    看解答，左右括號有先後順序的差別

--- Valid Parentheses/Valid Parentheses/Tests.cs
+++ Valid Parentheses/Valid Parentheses/Tests.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
+
+
 
 namespace ValidParentheses
 {
@@ -18,25 +21,71 @@ namespace ValidParentheses
         {
             Assert.AreEqual(false, Parentheses.ValidParentheses(")(((("));
         }
-    }
 
+        [Test]
+        public void SampleTest3()
+        {
+            Assert.AreEqual(false, Parentheses.ValidParentheses("))()()(()()()(((()()()()(()()"));
+        }
+
+        [Test]    //左右括號一定要按照順序
+        public void SampleTest4()
+        {
+            Assert.AreEqual(false, Parentheses.ValidParentheses(")("));
+        }
+    }
+    
     public class Parentheses
     {
+        //solution
         public static bool ValidParentheses(string input)
         {
+            var open = new Stack<char>();
+
+            foreach (var c in input)
             {
-                string[] inputElement = input.Select(o => o.ToString()).Distinct().ToArray();
-                int inputEqualLeft = input.Where(o => o.ToString() == "(").Count();
-                int inputEqualRight = input.Where(o => o.ToString() == ")").Count();
-                if (inputEqualLeft == inputEqualRight)
+                switch (c)
                 {
-                    return true;
-                }
-                else
-                {
-                    return false;
+                    case '(':              
+                        open.Push(c);
+                        break;
+
+                    case ')':  
+                        if (open.Count == 0) 
+                            return false; 
+                        open.Pop();
+                        break;
                 }
             }
+            
+            return open.Count == 0;
+        } 
+        // solution
+        public static bool ValidParentheses1(string input)
+        {
+            int counter = 0;
+            foreach (char ch in input)
+            {
+                if (ch=='(')
+                {
+                    counter++;
+                    continue;
+                }
+                if (ch==')')
+                {
+                    counter--;
+                    if (counter<0)
+                    {
+                        return false;
+                    }
+                    continue;
+                }
+            }
+            if (counter != 0)
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
