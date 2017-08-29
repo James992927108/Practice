b3ecf9772448cf72898dcca5f51685fd83fbb1d2
Jazerzxc <james992927108@gmail.com>
2017-08-29 16:53:10 +0800

    for pull

--- 19_Merged_String_Checker/19_Merged_String_Checker/Test.cs
+++ 19_Merged_String_Checker/19_Merged_String_Checker/Test.cs
@@ -24,30 +24,37 @@ namespace _Merged_String_Checker
 		{
 			Assert.IsFalse(StringMerger.isMerge("codewars", "cod", "wars"), "Codewars are not codwars");
 		}
-	public class StringMerger
-	{
-		public static bool isMerge(String s, String part1, String part2) {
-			if (part1 == part2 & s!="") return false;
 
-			string[] get_s_element = s.Select(o => o.ToString()).ToArray();
-			string[] get_part1_element = part1.Select(o => o.ToString()).ToArray();
-			string[] get_part2_element = part2.Select(o => o.ToString()).ToArray();
+		public class StringMerger
+		{
+			public static bool isMerge(String s, String part1, String part2)
+			{
+				if (part1 == part2 & s != "") return false;
 
-			int l = get_s_element.Length, l1 = get_part1_element.Length, l2 = get_part2_element.Length;
-			int n = 0, m = 0;
-			if (l == (l1 + l2)) {
-				for (int i = 0; i < l; i++) {
-					if (n < l1 && get_s_element[i] == get_part1_element[n]) n++;
-					if (m < l2 && get_s_element[i] == get_part2_element[m]) m++;
+				string[] get_s_element = s.Select(o => o.ToString()).ToArray();
+				string[] get_part1_element = part1.Select(o => o.ToString()).ToArray();
+				string[] get_part2_element = part2.Select(o => o.ToString()).ToArray();
+
+				int l = get_s_element.Length, l1 = get_part1_element.Length, l2 = get_part2_element.Length;
+				int n = 0, m = 0;
+				if (l == (l1 + l2))
+				{
+					for (int i = 0; i < l; i++)
+					{
+						if (n < l1 && get_s_element[i] == get_part1_element[n]) n++;
+						if (m < l2 && get_s_element[i] == get_part2_element[m]) m++;
+					}
+					if (l == n + m)
+					{
+						return true;
+					}
+					else return false;
+
 				}
-				if (l == n + m) {
-					return true;
-				} else return false;
-
-			} else return false;
+				else return false;
+			}
 		}
 	}
-	
 }
 
 
---
+++ Valid Parentheses/Valid Parentheses/Tests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace ValidParentheses
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void SampleTest1()
+        {
+            Assert.AreEqual(true, Parentheses.ValidParentheses("()"));
+        }
+
+        [Test]
+        public void SampleTest2()
+        {
+            Assert.AreEqual(false, Parentheses.ValidParentheses(")(((("));
+        }
+    }
+
+    public class Parentheses
+    {
+        public static bool ValidParentheses(string input)
+        {
+            {
+                string[] inputElement = input.Select(o => o.ToString()).Distinct().ToArray();
+                int inputEqualLeft = input.Where(o => o.ToString() == "(").Count();
+                int inputEqualRight = input.Where(o => o.ToString() == ")").Count();
+                if (inputEqualLeft == inputEqualRight)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
