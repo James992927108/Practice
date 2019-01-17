7d429cefc6f1ad1c0041df11e09aaf83ade5ff6a
Jazerzxc <james992927108@gmail.com>
2019-01-17 16:09:54 +0800

    1_Convert boolean values

---
+++ python/1_Convert boolean values/bool_to_word_test.py
@@ -0,0 +1,17 @@
+import sys
+
+sys.dont_write_bytecode = True
+
+import unittest
+
+import bool_to_word
+
+class Test_TestIncrementDecrement(unittest.TestCase):
+    def test_true(self):
+        self.assertEquals(bool_to_word.bool_to_word(True), 'Yes')
+
+    def test_false(self):
+        self.assertEquals(bool_to_word.bool_to_word(False), 'No')
+
+if __name__ == '__main__':
+    unittest.main()
