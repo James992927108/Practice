a9e75c2ddeb38358c1715e163b04f6fdbae730b9
Jazerzxc <james992927108@gmail.com>
2022-06-09 00:12:56 +0800

    update

--- HackerRank/7_Number_Line_Jumps.py
+++ HackerRank/7_Number_Line_Jumps.py
@@ -21,8 +21,24 @@ def kangaroo(x1, v1, x2, v2):
     # Write your code here
     if x1 < x2 and v1 < v2:
         return "NO"
-    first = x1 + v1
-    second = x2 + v2
+    
+    if v1 - v2 == 0:
+        return "NO"
+
+    count = (x2 - x1) / (v1 - v2)
+
+    if count >= 1:
+        if count % 1 == 0: # check is integer
+            count = int(count)
+            return "YES"
+        else:
+            # maybe 2.xxx
+            return "NO"
+    else:
+        # maybe 0.xxxx
+        return "NO"
+
+
 
 
 if __name__ == '__main__':
@@ -37,11 +53,20 @@ if __name__ == '__main__':
     # x2 = int(first_multiple_input[2])
 
     # v2 = int(first_multiple_input[3])
-    x1 = 0
-    v1 = 3
-    x2 = 4
-    v2 = 2
-    result = kangaroo(x1, v1, x2, v2)
+    # x1 = 0
+    # v1 = 3
+    # x2 = 4
+    # v2 = 2
+    # x1 = 1817
+    # v1 = 9931
+    # x2 = 8417
+    # v2 = 190
+    # 21 6 47 3
+    # result = kangaroo(0, 3, 4, 2)
+
+    # result = kangaroo(21,6, 47, 3)
+    result = kangaroo(43, 2, 70, 2)
+    print(result)
 
     # fptr.write(result + '\n')
 
