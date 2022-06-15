a9ce3791f243edea72ac761974218a312b36c334
Jazerzxc <james992927108@gmail.com>
2022-06-15 22:23:45 +0800

    update

---
+++ Leetcode/20_Valid_Parentheses/ValidParentheses.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+bool isValid(char * s){
+
+}
+
+int main()
+{
+    char* strs[] ={"flower","flow","flight"};
+    s = "()"
+    // char** temp;
+    // temp = &strs[0];
+    // printf("%s\n", *temp);
+
+    int strsSize = 3;
+    char* result;
+
+    result = longestCommonPrefix(strs, strsSize);
+
+    printf("%s\n", result);
+    return 0;
+}
